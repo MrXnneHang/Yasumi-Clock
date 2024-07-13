6b961375b7cf987506cc77de1ea903da5189e2a0
MrXnneHang <XnneHang@gmail.com>
2024-07-13 18:35:54 +0800

    完成贴图部分

--- yasumi_clock.py
+++ yasumi_clock.py
@@ -1,20 +1,30 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QPixmap, QImage,QIcon
 import pyautogui
 from PIL import Image
 import numpy as np
 from util import load_config
 from yasumi_draw_rec import ManualSelectionWindow
 from qfluentwidgets import PrimaryPushButton
-
+import PIL
 class Main_Window_UI(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.config = load_config("./yasumi_config.yml")
-        self.main_window = self.config["yasumi_clock"]["main_window"]
+        self.window_config = load_config("./yasumi_config.yml")
+        self.src_config = load_config("./src.yml")
+
+        # Window pos
+        self.main_window = self.window_config["yasumi_clock"]["main_window"]
         self.main_window_pos = self.main_window["window_pos"]
         self.draw_button_pos = self.main_window["start_draw"]
         self.start_fanqie_pos = self.main_window["start_fanqie"]
+        self.animation_pos = self.main_window["animation"]
+
+        # Image Source
+        self.example_img_path = self.src_config["example"]
+
+        # Init
         self.initUI()
     def initUI(self):
         self.setWindowTitle('Main Window')
@@ -23,7 +33,8 @@ class Main_Window_UI(QtWidgets.QWidget):
                          self.main_window_pos[2],
                          self.main_window_pos[3],)
         
-        self.startdrawButton = PrimaryPushButton('draw_main_window', self)
+        # Buttons
+        self.startdrawButton = PrimaryPushButton('Draw Main Window', self)
         self.startdrawButton.setGeometry(self.draw_button_pos[0],
                                          self.draw_button_pos[1],
                                          self.draw_button_pos[2],
@@ -33,6 +44,31 @@ class Main_Window_UI(QtWidgets.QWidget):
                                            self.start_fanqie_pos[1],
                                            self.start_fanqie_pos[2],
                                            self.start_fanqie_pos[3])
+        
+        # Labels
+        self.Animation_Label = QtWidgets.QLabel(self)
+        self.Animation_Label.setGeometry(QtCore.QRect(self.animation_pos[0],
+                                                      self.animation_pos[1],
+                                                      self.animation_pos[2],
+                                                      self.animation_pos[3]))
+        self.Animation_Label.setText("")
+        self.Animation_Label.setObjectName("Animation")
+
+        # Func
+        self.Draw_Image(Label=self.Animation_Label,
+                        path=self.example_img_path,
+                        Pos=self.animation_pos)
+    def Draw_Image(self,Label,path,Pos):
+        img = PIL.Image.open(path)
+        img = img.resize((Pos[2],Pos[3]),Image.BILINEAR)
+        img = np.array(img)
+        rgb_image = img
+        h, w, ch = rgb_image.shape
+        bytes_per_line = ch * w
+        q_img = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(q_img)
+        Label.setPixmap(pixmap)
+
 
 class Main_Window_Response(Main_Window_UI):
     def __init__(self):
