c3fddf2ce16cebec6bfadf0619590374a7e24cc1
MrXnneHang <XnneHang@gmail.com>
2024-07-14 12:18:23 +0800

    加入用来画gif的线程

--- MainWindowThread.py
+++ MainWindowThread.py
@@ -13,18 +13,33 @@ class DrawAnimationThread(QThread):
     def __init__(self):
         super().__init__()
     
-    def setup(self,path, label, pos, frame_speed=24):
+    def setup(self,path, label, pos, frame_speed=24,whileTrue=True):
         self.path = path
         self.label = label
         self.pos = pos
         self.frame_speed = frame_speed
+        self.whileTrue = whileTrue
     def run(self):
         self.running = True
         frames = split_gif_to_frames(self.path)
-        while self.running:
+
+        if self.whileTrue:
+        # 循环播放
+            while self.running:
+                for frame in frames:
+                    if not self.running:
+                        print("线程已经正常退出")
+                        return
+                    rgb_image = frame.resize((self.pos[2],self.pos[3]),Image.BILINEAR)
+                    rgb_image = np.array(rgb_image)
+                    h, w, ch = rgb_image.shape
+                    bytes_per_line = ch * w
+                    q_img = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format_RGB888)
+                    pixmap = QPixmap.fromImage(q_img)
+                    self.label.setPixmap(pixmap)
+                    sleep(1 / self.frame_speed)
+        else:
             for frame in frames:
-                if not self.running:
-                    return
                 rgb_image = frame.resize((self.pos[2],self.pos[3]),Image.BILINEAR)
                 rgb_image = np.array(rgb_image)
                 h, w, ch = rgb_image.shape
@@ -33,3 +48,10 @@ class DrawAnimationThread(QThread):
                 pixmap = QPixmap.fromImage(q_img)
                 self.label.setPixmap(pixmap)
                 sleep(1 / self.frame_speed)
+            while self.running:
+                if not self.running:
+                    print("线程已经正常退出")
+                    return
+    def stop(self):
+        self.running = False
+        self.quit()
