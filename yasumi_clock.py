1f3353f2950b38ae32c0cb5b96d63ebea1d8c636
MrXnneHang <XnneHang@gmail.com>
2024-07-14 22:03:09 +0800

    加入加减时长，reset的功能

--- yasumi_clock.py
+++ yasumi_clock.py
@@ -28,6 +28,10 @@ class Main_Window_UI(QtWidgets.QWidget):
         self.start_fanqie_pos = self.main_window["start_fanqie"]
         self.animation_pos = self.main_window["animation"]
         self.timer_pos = self.main_window["timer"]
+        self.addTime_pos = self.main_window["add_time"]
+        self.subTime_pos = self.main_window["sub_time"]
+        self.setTime_pos = self.main_window["set_time"]
+        self.resetTime_pos = self.main_window["reset_time"]
 
         # Image Source
         self.example_img_path = self.src_config["example"]
@@ -54,6 +58,22 @@ class Main_Window_UI(QtWidgets.QWidget):
                                                         self.start_fanqie_pos[1],
                                                         self.start_fanqie_pos[2],
                                                         self.start_fanqie_pos[3]))
+        self.addTimeButton = PrimaryPushButton('➕',self)
+        self.addTimeButton.setGeometry(QtCore.QRect(self.addTime_pos[0],
+                                                    self.addTime_pos[1],
+                                                    self.addTime_pos[2],
+                                                    self.addTime_pos[3]))
+        self.subTimeButton = PrimaryPushButton('➖',self)
+        self.subTimeButton.setGeometry(QtCore.QRect(self.subTime_pos[0],
+                                                    self.subTime_pos[1],
+                                                    self.subTime_pos[2],
+                                                    self.subTime_pos[3]))
+        self.resetTimeButton = PrimaryPushButton('Rest',self)
+        self.resetTimeButton.setGeometry(QtCore.QRect(self.resetTime_pos[0],
+                                                      self.resetTime_pos[1],
+                                                      self.resetTime_pos[2],
+                                                      self.resetTime_pos[3]))
+
 
         # Labels
         self.animation_label = QtWidgets.QLabel(self)
@@ -68,16 +88,35 @@ class Main_Window_UI(QtWidgets.QWidget):
             QLabel {
                 font-size: 30px;
                 font-weight: bold;
-                color: rgba(255, 255, 255, 1);  /* White color */
-                background-color: rgba(52, 152, 219, 1);  /* Blue color */
+                color: rgba(0, 0, 0, 1);  /* White color */
                 padding: 10px;
                 border-radius: 5px;
+                font-family: Arial;  /* 设置字体为 Arial */
+                                     
             }
         """)
         self.timeLabel.setGeometry(QtCore.QRect(self.timer_pos[0],
-                                                      self.timer_pos[1],
-                                                      self.timer_pos[2],
-                                                      self.timer_pos[3]))
+                                                self.timer_pos[1],
+                                                self.timer_pos[2],
+                                                self.timer_pos[3]))
+
+        self.setTimeLabel = QtWidgets.QLabel(self)
+                # Set font size, weight, and color using RGBA
+        self.setTimeLabel.setStyleSheet("""
+            QLabel {
+                font-size: 15px;
+                font-weight: bold;
+                color: rgba(0, 0, 0, 1);  /* White color */
+                padding: 10px;
+                border-radius: 5px;
+                font-family: Arial;  /* 设置字体为 Arial */
+                                     
+            }
+        """)
+        self.setTimeLabel.setGeometry(QtCore.QRect(self.setTime_pos[0],
+                                                   self.setTime_pos[1],
+                                                   self.setTime_pos[2],
+                                                   self.setTime_pos[3]))
 
         # Start thread to draw GIF frames
 
@@ -119,19 +158,56 @@ class Main_Window_Response(Main_Window_UI):
         super().__init__()
         self.startdrawButton.clicked.connect(self.showDrawMainWindow)
         self.startFanqieButton.clicked.connect(self.startFanqie)
+        self.addTimeButton.clicked.connect(self.add_time)
+        self.subTimeButton.clicked.connect(self.sub_time)
+        self.resetTimeButton.clicked.connect(self.resetTime)
         self.loadingwindow = loading_window
+
         self.timeRemaining = QTime(0,0)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.updateTimer)
-        self.timeLabel.setText("Begin!")
+        self.closeYasumi = QTimer(self)
         
+
+        self.timeLabel.setText("Begin!")
+        self.total_time = ["00:00","05:00","10:00",
+                           "15:00","20:00","25:00",
+                           "30:00","35:00","40:00"]
+        self.time_index = 4
+        self.setTimeLabel.setText(self.total_time[self.time_index])
+
+        self.timerRunning = False
+
+    
+    def add_time(self):
+        if self.time_index < 8:
+            self.time_index += 1
+            self.setTimeLabel.setText(self.total_time[self.time_index])
+        else:
+            pass
+    def sub_time(self):    
+        if self.time_index > 0:
+            self.time_index -= 1
+            self.setTimeLabel.setText(self.total_time[self.time_index])
+        else:
+            pass
+    def resetTime(self):
+        self.timerRunning = False
+        self.timer.stop()
+        self.timeLabel.setText("Reset")
+
 
     def showDrawMainWindow(self):
         child_window_pos = self.list_main_button_pos()
         self.selectionWindow = ManualSelectionWindow(self.main_window_pos,child_window_pos)
         self.selectionWindow.show()
     def startFanqie(self):
-        self.startCountdown("00:05")
+        if not self.timerRunning:
+            self.startCountdown(self.total_time[self.time_index])
+            self.timerRunning = True
+        else:
+            print("已经有计时器在运行")
+            pass
        
 
     
@@ -154,6 +230,8 @@ class Main_Window_Response(Main_Window_UI):
             self.timeLabel.setText("End!")
             self.yasumi = yasumiWindow()
             self.yasumi.show()
+            self.timerRunning = False
+            self.closeYasumi.singleShot(5000,self.yasumi.close)
         else:
             self.timeRemaining = self.timeRemaining.addSecs(-1)
             self.timeLabel.setText(self.timeRemaining.toString("mm:ss"))
@@ -164,7 +242,10 @@ class Main_Window_Response(Main_Window_UI):
             self.loadingwindow.close()
 
     def list_main_button_pos(self):
-        return [self.draw_button_pos,self.start_fanqie_pos,self.animation_pos]
+        return [self.draw_button_pos,self.start_fanqie_pos,self.animation_pos,
+                self.timer_pos,self.addTime_pos,self.subTime_pos,
+                self.resetTime_pos,self.setTime_pos
+                ]
 
 
 
